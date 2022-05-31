4ca4e9401f47a63400ef4aa9d710f49417705e2a
sammyfe <oluwayanmife2@gmail.com>
2022-05-31 16:42:11 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 100):
-    print('{:02d}'.format(i), end='\n' if i == 99 else ', ')
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
