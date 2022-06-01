c0c18a1d46fd58e6671828e0e7c0aa7ed0829ee3
sammyfe <oluwayanmife2@gmail.com>
2022-05-31 18:42:34 -0700

    m

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+for i in range(0, 99):
+    print("{} = 0x{:x}".format(i, i))
