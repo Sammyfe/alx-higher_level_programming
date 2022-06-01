5f79c3f401bfb25e7b7bd9a29defd86f54e0cb70
sammyfe <oluwayanmife2@gmail.com>
2022-05-31 18:46:00 -0700

    changed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{} = 0x{:x}".format(i, i))
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
