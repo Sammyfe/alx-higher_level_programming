a3ac9131fd37487051aac8b1fcb13a135d07d571
sammyfe <oluwayanmife2@gmail.com>
2022-05-31 17:21:27 -0700

    m

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+for a in range(122, 96, -1):
+    if a % 2 != 0:
+        print("{:c}".format(a - 32), end='')
+    else:
+        print("{:c}".format(a), end='')
