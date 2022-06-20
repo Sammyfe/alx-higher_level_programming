ac44b2b87db184ab4017d062dfa73ddbfd9c5365
sammyfe <oluwayanmife2@gmail.com>
2022-06-20 16:09:38 -0700

    m

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for s in range(2, 5):
+        try:
+            if s > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / s
+        except:
+            result = b + a
+            break
+    return result
